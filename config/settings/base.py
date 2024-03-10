a7dea47a0339cb05b756e29171f5fc6f16d7555a
XakimovIslom <68080722+XakimovIslom@users.noreply.github.com>
2024-03-10 16:14:23 +0500

    Update base.py
    fix: SECRET_KEY and DEBUG with .env file

--- config/settings/base.py
+++ config/settings/base.py
@@ -16,10 +16,12 @@ env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "1"
+# SECRET_KEY = "1"
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = env.bool("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
